5f0f12b0ae863eb37a42db66596af505a0895c59
Naman P <66672786+HyperLynk@users.noreply.github.com>
2021-03-01 14:57:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ if (!location.hash) {
   const roomName = 'observable-' + roomHash;
   const configuration = {
     iceServers: [{
-        urls: ['stun:stun.l.google.com:19302','turn:numb.viagenie.ca'],
-        credential: 'naman',
-        username: 'naman.patel@boothamstudent.co.uk'    
+        urls: ['stun:stun.l.google.com:19302','turn:192.158.29.39:3478?transport=udp'],
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'    
     }]
   };
   let room;
