758b3117916c4451a4de60f18e784ce1e66aec37
Hyperlynker <66867572+HyperLynker@users.noreply.github.com>
2021-03-01 14:27:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ if (!location.hash) {
   const roomName = 'observable-' + roomHash;
   const configuration = {
     iceServers: [{
-        urls: 'stun:stun.l.google.com:19302',
-        urls: 'turn:numb.viagenie.ca',
+        urls: ['stun:stun.l.google.com:19302','turn:numb.viagenie.ca'],
         credential: 'muazkh',
         username: 'webrtc@live.com'    
     }]
