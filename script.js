3cc60efe62886b3047fcbf791193bbc191c72555
Hyperlynker <66867572+HyperLynker@users.noreply.github.com>
2021-03-01 13:59:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ if (!location.hash) {
   const configuration = {
     iceServers: [{
       urls: 'stun:stun.l.google.com:19302',
-      urls: 'turn:turn01.hubl.in?transport=udp'
+      url: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
     }]
   };
   let room;
